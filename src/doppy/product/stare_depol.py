79d6dd3fab9fe8f809fc506650e27286d1902eaa
Niko Leskinen <niko.leskinen@fmi.fi>
2024-08-21 13:32:07 +0300

    add beta_cross to stare depol product

--- src/doppy/product/stare_depol.py
+++ src/doppy/product/stare_depol.py
@@ -1,9 +1,18 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from io import BufferedIOBase
+from pathlib import Path
+from typing import Sequence
+
 import numpy as np
 import numpy.typing as npt
 
+from doppy import options
 from doppy.product.stare import Stare
 
 
+@dataclass
 class StareDepol:
     """
     Stare product with depolarisation ratio derived from co-polarised and
@@ -21,6 +30,9 @@ class StareDepol:
     beta
         An array of backscatter coefficients for the co-polarised signal, in
         sr-1 m-1.
+    beta_cross
+        An array of backscatter coefficients for the cross-polarised signal, in
+        sr-1 m-1.
     radial_velocity
         An array of radial velocities of the co-polarised signal, in m s-1.
     mask
@@ -33,26 +45,55 @@ class StareDepol:
     system_id
         A string identifier for the lidar.
 
+
     Raises
     ------
     ValueError
         If the input `co` and `cross` products have mismatched wavelengths,
         system IDs, radial distances, or elevation angles, this exception is
         raised.
+
+
+    References
+    ----------
+    Aerosol particle depolarization ratio at 1565 nm measured with a Halo Doppler lidar
+        authors: Ville Vakkari, Holger Baars, Stephanie Bohlmann, Johannes Bühl,
+            Mika Komppula, Rodanthi-Elisavet Mamouri, and Ewan James O'Connor
+        doi: https://doi.org/10.5194/acp-21-5807-2021
     """
 
     time: npt.NDArray[np.datetime64]
     radial_distance: npt.NDArray[np.float64]
     elevation: npt.NDArray[np.float64]
     beta: npt.NDArray[np.float64]
+    beta_cross: npt.NDArray[np.float64]
     radial_velocity: npt.NDArray[np.float64]
     mask: npt.NDArray[np.bool_]
     depolarisation: npt.NDArray[np.float64]
+    mask_depolarisation: npt.NDArray[np.bool_]
+    mask_beta_cross: npt.NDArray[np.bool_]
+    polariser_bleed_through: float
     wavelength: float
     system_id: str
 
-    def __init__(self, co: Stare, cross: Stare):
-        if co.wavelength != cross.wavelength:
+    def __init__(
+        self,
+        co: Stare,
+        cross: Stare,
+        polariser_bleed_through: float = 0.0,
+    ):
+        """
+        Parameters
+        ----------
+        co: Stare
+            The co-polarised data.
+        cross: Stare
+            The cross-polarised data. The `cross.time` array is expected to be sorted.
+        polariser_bleed_through: float, default=0.0
+            The amount of bleed-through from the polariser.
+        """
+
+        if not np.isclose(co.wavelength, cross.wavelength):
             raise ValueError(
                 "Different wavelength in co and cross: "
                 f"{co.wavelength} vs {cross.wavelength}"
@@ -65,19 +106,75 @@ class StareDepol:
         if not np.allclose(co.radial_distance, cross.radial_distance, atol=1):
             raise ValueError("Different radial distance in co and cross")
 
-        time_ind = np.argmin(np.abs(co.time - cross.time[:, np.newaxis]), axis=0)
-        cross_elevation = cross.elevation[time_ind]
-        cross_beta = cross.beta[time_ind, :]
+        if co.beta.shape[1] != cross.beta.shape[1]:
+            raise ValueError(
+                "Range dimension mismatch in co and cross: "
+                f"{co.beta.shape[1]} vs {cross.beta.shape[1]}"
+            )
 
-        if not np.allclose(co.elevation, cross_elevation, atol=1):
+        ind = np.searchsorted(cross.time, co.time, side="left")
+        pick_ind = ind < len(cross.time)
+        time_diff_threshold = 2 * np.median(np.diff(co.time))
+        co_cross_timediff_below_threshold = (
+            cross.time[ind[pick_ind]] - co.time[pick_ind] < time_diff_threshold
+        )
+        pick_ind[pick_ind] &= co_cross_timediff_below_threshold
+
+        if not np.allclose(
+            co.elevation[pick_ind], cross.elevation[ind[pick_ind]], atol=1
+        ):
             raise ValueError("Different elevation in co and cross")
+
+        depolarisation = np.full_like(co.beta, np.nan)
+        co_beta = co.beta[pick_ind]
+        depolarisation[pick_ind] = (
+            cross.beta[ind[pick_ind]] - polariser_bleed_through * co_beta
+        ) / co_beta
+        cross_beta = np.full_like(co.beta, np.nan)
+        cross_beta[pick_ind] = cross.beta[ind[pick_ind]]
 
         self.time = co.time
         self.radial_distance = co.radial_distance
         self.elevation = co.elevation
         self.beta = co.beta
+        self.beta_cross = cross_beta
         self.radial_velocity = co.radial_velocity
         self.mask = co.mask
-        self.depolarisation = cross_beta / co.beta
+        self.depolarisation = depolarisation
+        self.mask_depolarisation = np.isnan(depolarisation)
+        self.mask_beta_cross = np.isnan(self.beta_cross)
+        self.polariser_bleed_through = polariser_bleed_through
         self.wavelength = co.wavelength
         self.system_id = co.system_id
+
+    @classmethod
+    def from_halo_data(
+        cls,
+        co_data: Sequence[str]
+        | Sequence[Path]
+        | Sequence[bytes]
+        | Sequence[BufferedIOBase],
+        co_data_bg: Sequence[str]
+        | Sequence[Path]
+        | Sequence[tuple[bytes, str]]
+        | Sequence[tuple[BufferedIOBase, str]],
+        cross_data: Sequence[str]
+        | Sequence[Path]
+        | Sequence[bytes]
+        | Sequence[BufferedIOBase],
+        cross_data_bg: Sequence[str]
+        | Sequence[Path]
+        | Sequence[tuple[bytes, str]]
+        | Sequence[tuple[BufferedIOBase, str]],
+        bg_correction_method: options.BgCorrectionMethod,
+        polariser_bleed_through: float = 0,
+    ) -> StareDepol:
+        co = Stare.from_halo_data(
+            data=co_data, data_bg=co_data_bg, bg_correction_method=bg_correction_method
+        )
+        cross = Stare.from_halo_data(
+            data=cross_data,
+            data_bg=cross_data_bg,
+            bg_correction_method=bg_correction_method,
+        )
+        return cls(co, cross, polariser_bleed_through)
